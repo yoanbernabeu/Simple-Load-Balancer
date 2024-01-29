21c5516129d5898801339ca5550a5bdc7f818ab3
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2024-01-29 23:22:00 +0100

    👌 IMPROVE: Dockerfile + entrypoint

--- LoadBalancer/entrypoint.sh
+++ LoadBalancer/entrypoint.sh
@@ -3,8 +3,11 @@
 # Check if the ENABLE_HTTPS variable is set to true
 if [ "$ENABLE_HTTPS" = "true" ]; then
     /usr/local/bin/renew-certs.sh
-    export DOMAIN_CERT_PATH="/etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem"
+    
+    export DOMAIN_CERT_PATH="/etc/letsencrypt/live/$DOMAIN_NAME/combined.pem"
+    cat /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem > /etc/letsencrypt/live/$DOMAIN_NAME/combined.pem
     envsubst < /usr/local/etc/haproxy/haproxy.cfg.https.template > /usr/local/etc/haproxy/haproxy.cfg
+    
     cron -f &
 else
     envsubst < /usr/local/etc/haproxy/haproxy.cfg.http.template > /usr/local/etc/haproxy/haproxy.cfg
